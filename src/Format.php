96db1e46e9892849a3d08390d0c3aef605344e6a
martin-stepwolf <15151130@aguascalientes.tecnm.mx>
2020-10-12 19:55:04 -0500

    Initial files and configuration

---
+++ src/Format.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Text; // the same name like in composer.json
+
+class Format //the same name like the file
+{
+    public static function upperText($value)
+    {
+        return strtoupper($value);
+    }
+}
